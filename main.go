39d4d513d60c6434a24cee41648b6c4c6e0d5d29
Firdaus Dwika <firdausdwika@gmail.com>
2022-11-21 19:04:22 +0700

    Publish Protocol Email

--- main.go
+++ main.go
@@ -1,141 +1,47 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"io"
-	"log"
-	"net/http"
-	"net/url"
-	"sync"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sns"
+
+	"fmt"
+	"os"
 )
 
-var sess = session.Must(session.NewSessionWithOptions(session.Options{
-	SharedConfigState: session.SharedConfigEnable,
-}))
-
-var snsService = sns.New(sess)
-
-var publicHttpEndpoint string
-
-type SNSEvent struct {
-	RequestType  *string `json:"Type"`
-	SubscribeURL *string `json:"SubscribeURL"`
-	Message      *string `json:"Message"`
-}
-
 func main() {
+	emailPtr := flag.String("e", "", "The email address of the user subscribing to the topic")
+	topicPtr := flag.String("t", "", "The ARN of the topic to which the user subscribes")
 
-	flag.StringVar(&publicHttpEndpoint, "endpoint", "", "-endpoint")
 	flag.Parse()
 
-	if publicHttpEndpoint == "" {
-		log.Fatal("Public Endpoint is required")
-		flag.PrintDefaults()
+	if *emailPtr == "" || *topicPtr == "" {
+		fmt.Println("You must supply an email address and topic ARN")
+		fmt.Println("Usage: go run SnsSubscribe.go -e EMAIL -t TOPIC-ARN")
+		os.Exit(1)
 	}
 
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	go startHttpServer(wg, ":8000")
-	subscribe(publicHttpEndpoint)
-	go publish()
+	// Initialize a session that the SDK will use to load
+	// credentials from the shared credentials file. (~/.aws/credentials).
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		Config:  aws.Config{Region: aws.String("ap-southeast-1")},
+		Profile: "dev",
+	}))
 
-	wg.Wait()
-}
+	svc := sns.New(sess)
 
-func startHttpServer(wg *sync.WaitGroup, port string) {
-	defer wg.Done()
-	server := http.Server{
-		Addr:    port,
-		Handler: handler(),
-	}
-	log.Println("starting server on port 8000")
-	err := server.ListenAndServe()
-	log.Fatal(err)
-}
-
-func handler() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			log.Println(err)
-		}
-
-		req := SNSEvent{}
-
-		if err := json.Unmarshal(body, &req); err != nil {
-			log.Println(err)
-			w.WriteHeader(500)
-			return
-		}
-
-		//confirmation request
-		if *req.RequestType == "SubscriptionConfirmation" {
-			confirm(req)
-			return
-		}
-
-		//messages
-		log.Println("Got Message", *req.Message)
-		w.WriteHeader(200)
-	}
-}
-
-// subscribe subscribes to the topic. This can be done from the AWS console also.
-func subscribe(httpURL string) {
-	inp := &sns.SubscribeInput{
-		Endpoint: aws.String(httpURL),
-		Protocol: aws.String("http"),
-		TopicArn: aws.String("arn:aws:sns:ap-southeast-1:716978518298:Order.fifo"),
-	}
-
-	output, err := snsService.Subscribe(inp)
+	result, err := svc.Subscribe(&sns.SubscribeInput{
+		Endpoint:              emailPtr,
+		Protocol:              aws.String("email"),
+		ReturnSubscriptionArn: aws.Bool(true), // Return the ARN, even if user has yet to confirm
+		TopicArn:              topicPtr,
+	})
 	if err != nil {
-		log.Println(err)
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
 
-	log.Println("Output", output.String())
-}
-
-func confirm(request SNSEvent) {
-
-	vals, err := url.ParseQuery(*request.SubscribeURL)
-	if err != nil {
-		log.Println(err)
-	}
-
-	token := vals.Get("Token")
-	topicARN := vals.Get("TopicArn")
-
-	confirm := &sns.ConfirmSubscriptionInput{
-		Token:    aws.String(token),
-		TopicArn: aws.String(topicARN),
-	}
-	output, err := snsService.ConfirmSubscription(confirm)
-	if err != nil {
-		log.Println(err)
-	}
-
-	log.Println("confirm output", output.String())
-}
-
-func publish() {
-	for {
-		input := &sns.PublishInput{
-			Message:  aws.String("Current time is " + time.Now().String()),
-			TopicArn: aws.String("arn:aws:sns:ap-southeast-1:716978518298:ClassOrder"),
-		}
-		//We are ignoring the output here. Its the messageid.
-		_, err := snsService.Publish(input)
-		if err != nil {
-			log.Println("publish error", err)
-		}
-
-		time.Sleep(2 * time.Second)
-	}
+	fmt.Println(*result.SubscriptionArn)
 }
